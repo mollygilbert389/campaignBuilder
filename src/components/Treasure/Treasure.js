6a2159fc82fa57790fd002c1dd2bc8f5b1777317
Molly Gilbert <msgilbert389@gmail.com>
2020-08-30 19:13:10 -0500

    adding cleaned up code to treasure card

--- src/components/Treasure/Treasure.js
+++ src/components/Treasure/Treasure.js
@@ -5,92 +5,85 @@ import {FormControlLabel, Checkbox, FormGroup, Slider} from '@material-ui/core'
 import "./style.css"
 
 class Treasure extends Component {
-
-    constructor() {
-        super();
-        this.state ={
-            showModal: false,
-            equipment: false, 
-            magicItem: false,
-            gems: false,
-            money: false,
-            tradeGoods: false,
-            justXp: false,
-            treasues: [], 
-            treasureNumber: 0,
-            suggestedTreasure: 0,
-        }
+    state ={
+        showModal: false,
+        equipment: false, 
+        magicItem: false,
+        gems: false,
+        money: false,
+        tradeGoods: false,
+        justXp: false,
+        treasues: [], 
+        treasureNumber: 0,
+        suggestedTreasure: 0,
     }
-        handleChange = (event) => {
-            this.setState({
-                [event.target.name]: event.target.checked 
-            });
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.checked 
+        });
+    }
+
+    handleClick = () => {
+        this.setState({
+            showModal: !this.state.showModal,
+        })
+        this.suggestMeTreasure()
+    }
+
+    close = () => {
+        let treasueHolder = []
+        const equipment = this.state.equipment
+        const magicItem = this.state.magicItem
+        const gems = this.state.gems
+        const money = this.state.money
+        const tradeGoods = this.state.tradeGoods
+        const justXp = this.state.justXp
+
+        if (equipment) {
+            treasueHolder = treasueHolder.concat("Equipment")
+        }
+        if (magicItem) {
+            treasueHolder = treasueHolder.concat("Magic Items")
+        }
+        if (gems) {
+            treasueHolder = treasueHolder.concat("Gems, Jewelry, Art")
+        }
+        if (money) {
+            treasueHolder = treasueHolder.concat("Money")
+        }
+        if (tradeGoods) {
+            treasueHolder = treasueHolder.concat("Trade Goods")
+        }
+        if (justXp) {
+            treasueHolder = treasueHolder.concat("XP Points")
         }
 
-        handleClick = () => {
-            this.setState({
-                showModal: !this.state.showModal,
-            })
+        this.setState({
+            showModal: !this.state.showModal,
+            treasues: treasueHolder,
+        }, () => {this.props.setTreasureType(this.state.treasues)})
+    }
 
-            this.suggestMeTreasure()
-        }
+    suggestMeTreasure = () =>{
+        let rooms = this.props.campaign.rooms
+        rooms = rooms / 2
+        rooms = Math.round(rooms)
+        rooms = rooms -1 
 
-        close = () => {
+        this.setState({
+            suggestedTreasure: rooms,
+            treasureNumber: rooms
+        }, () => this.props.setTreasureNumber(this.state.treasureNumber))
+    }
 
-            let treasueHolder = []
-            const equipment = this.state.equipment
-            const magicItem = this.state.magicItem
-            const gems = this.state.gems
-            const money = this.state.money
-            const tradeGoods = this.state.tradeGoods
-            const justXp = this.state.justXp
-
-            if (equipment) {
-                treasueHolder = treasueHolder.concat("Equipment")
-            }
-            if (magicItem) {
-                treasueHolder = treasueHolder.concat("Magic Items")
-            }
-            if (gems) {
-                treasueHolder = treasueHolder.concat("Gems, Jewelry, Art")
-            }
-            if (money) {
-                treasueHolder = treasueHolder.concat("Money")
-            }
-            if (tradeGoods) {
-                treasueHolder = treasueHolder.concat("Trade Goods")
-            }
-            if (justXp) {
-                treasueHolder = treasueHolder.concat("XP Points")
-            }
-
-            this.setState({
-                showModal: !this.state.showModal,
-                treasues: treasueHolder,
-            }, () => {this.props.setTreasureType(this.state.treasues)})
-
-            
-        }
-
-        suggestMeTreasure = () =>{
-            let rooms = this.props.campaign.rooms
-            rooms = rooms / 2
-            rooms = Math.round(rooms)
-            rooms = rooms -1 
-
-            this.setState({
-                suggestedTreasure: rooms,
-                treasureNumber: rooms
-            }, () => this.props.setTreasureNumber(this.state.treasureNumber))
-        }
-
-        handleSlider = (event, value) => {
-            console.log(value)
-            this.setState({
-                treasureNumber: value
-            }, () => {this.props.setTreasureNumber(this.state.treasureNumber)}
-            )
-        }
+    handleSlider = (event, value) => {
+        console.log(value)
+        this.setState({
+            treasureNumber: value
+        }, () => {this.props.setTreasureNumber(this.state.treasureNumber)}
+        )
+    }
 
 render() {  
 
@@ -105,105 +98,104 @@ render() {
 
     return (
         <div>
-        <div className="btns">
-        <Button id="questGiver" variant="outline-success" size="lg" onClick={this.handleClick}>Treasure
-        </Button>
+            <div className="btns">
+                <Button variant="outline-success" size="lg" onClick={this.handleClick}>Treasure
+                </Button>
             </div>
-        <Modal show={this.state.showModal}>
-        <Modal.Dialog id="villianbtn">
-            <Modal.Header>
-                <Modal.Title>Let's talk Treasure!</Modal.Title>
-            </Modal.Header>
+            <Modal show={this.state.showModal}>
+            <Modal.Dialog>
+                <Modal.Header>
+                    <Modal.Title>Let's talk Treasure!</Modal.Title>
+                </Modal.Header>
 
-            <Modal.Body>
-                <p>Below are some buttons to help create the tresure and experience earned for your game!</p>
-                <p>You have selected {campaign.rooms} rooms and a short campaign. Due to this reason we suggest you have {this.state.suggestedTreasure} treasures excluding the quest price. Please check the boxes below of items you'd like to include and we'll generate them for you.</p>
+                <Modal.Body>
+                    <p>Below are some buttons to help create the tresure and experience earned for your game!</p>
+                    <p>You have selected {campaign.rooms} rooms and a short campaign. Due to this reason we suggest you have {this.state.suggestedTreasure} treasures excluding the quest price. Please check the boxes below of items you'd like to include and we'll generate them for you.</p>
+                    <br></br>
 
-                <br></br>
+                    <Slider
+                    min={1}
+                    max={10}
+                    defaultValue={this.state.suggestedTreasure}
+                    onChangeCommitted={this.handleSlider}
+                    valueLabelDisplay="on"
+                    />
 
-                <Slider
-                min={1}
-                max={10}
-                defaultValue={this.state.suggestedTreasure}
-                onChangeCommitted={this.handleSlider}
-                valueLabelDisplay="on"
-                />
+                    <FormGroup>
+                    <FormControlLabel disabled={(justXp) ? true: false}
+                    control={
+                    <Checkbox 
+                    checked={equipment} 
+                    onChange={this.handleChange} 
+                    name="equipment"
+                    value="Equipment" 
+                    color="primary"/>}
+                    label="Equipment"
+                    />
 
-                <FormGroup>
-                <FormControlLabel disabled={(justXp) ? true: false}
-                control={
-                <Checkbox 
-                checked={equipment} 
-                onChange={this.handleChange} 
-                name="equipment"
-                value="Equipment" 
-                color="primary"/>}
-                label="Equipment"
-                />
+                    <FormControlLabel disabled={(justXp) ? true: false}
+                    control={
+                    <Checkbox 
+                    checked={magicItem} 
+                    onChange={this.handleChange} 
+                    name="magicItem"
+                    value="Magic Item" 
+                    color="primary"/>}
+                    label="Magic Items"
+                    />
 
-                <FormControlLabel disabled={(justXp) ? true: false}
-                control={
-                <Checkbox 
-                checked={magicItem} 
-                onChange={this.handleChange} 
-                name="magicItem"
-                value="Magic Item" 
-                color="primary"/>}
-                label="Magic Items"
-                />
+                    <FormControlLabel disabled={(justXp) ? true: false}
+                    control={
+                    <Checkbox 
+                    checked={gems} 
+                    onChange={this.handleChange} 
+                    name="gems"
+                    value="Gems, Jewelry, Art" 
+                    color="primary"/>}
+                    label="Gems, Jewelry, Art"
+                    />
 
-                <FormControlLabel disabled={(justXp) ? true: false}
-                control={
-                <Checkbox 
-                checked={gems} 
-                onChange={this.handleChange} 
-                name="gems"
-                value="Gems, Jewelry, Art" 
-                color="primary"/>}
-                label="Gems, Jewelry, Art"
-                />
+                    <FormControlLabel disabled={(justXp) ? true: false}
+                    control={
+                    <Checkbox 
+                    checked={tradeGoods} 
+                    onChange={this.handleChange} 
+                    name="tradeGoods" 
+                    value="Trade Goods"
+                    color="primary"/>}
+                    label="Trade Goods"
+                    />
 
-                <FormControlLabel disabled={(justXp) ? true: false}
-                control={
-                <Checkbox 
-                checked={tradeGoods} 
-                onChange={this.handleChange} 
-                name="tradeGoods" 
-                value="Trade Goods"
-                color="primary"/>}
-                label="Trade Goods"
-                />
+                    <FormControlLabel disabled={(justXp) ? true: false}
+                    control={
+                    <Checkbox 
+                    checked={money} 
+                    onChange={this.handleChange} 
+                    name="money" 
+                    value="Money"
+                    color="primary"/>}
+                    label="Money"
+                    />
 
-                <FormControlLabel disabled={(justXp) ? true: false}
-                control={
-                <Checkbox 
-                checked={money} 
-                onChange={this.handleChange} 
-                name="money" 
-                value="Money"
-                color="primary"/>}
-                label="Money"
-                />
+                    <FormControlLabel disabled={(tradeGoods || gems || magicItem || equipment || money) ? true: false}
+                    control={
+                    <Checkbox 
+                    checked={justXp} 
+                    onChange={this.handleChange} 
+                    name="justXp" 
+                    value="XP Points"
+                    color="primary"/>}
+                    label="Just XP"
+                    />
 
-                <FormControlLabel disabled={(tradeGoods || gems || magicItem || equipment || money) ? true: false}
-                control={
-                <Checkbox 
-                checked={justXp} 
-                onChange={this.handleChange} 
-                name="justXp" 
-                value="XP Points"
-                color="primary"/>}
-                label="Just XP"
-                />
+                </FormGroup>
+                </Modal.Body>
 
-            </FormGroup>
-            </Modal.Body>
-
-            <Modal.Footer>
-                <Button variant="outline-success" onClick={this.close}> Save</Button>
-            </Modal.Footer>
-        </Modal.Dialog>
-        </Modal>
+                <Modal.Footer>
+                    <Button variant="outline-success" onClick={this.close}> Save</Button>
+                </Modal.Footer>
+            </Modal.Dialog>
+            </Modal>
         </div>
     );
 }
