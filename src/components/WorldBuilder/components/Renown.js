d065fa2eabf950618b4ef92a04e2c047290aef67
Molly Gilbert <msgilbert389@gmail.com>
2021-11-12 16:20:42 -0600

    refator renown

--- src/components/WorldBuilder/components/Renown.js
+++ src/components/WorldBuilder/components/Renown.js
@@ -1,120 +1,102 @@
-import React, {Component} from 'react'
-import Modal from 'react-bootstrap/Modal'
-import {Button, Dropdown, Form, FormControl} from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {FormControlLabel, Checkbox} from '@material-ui/core'
-import "../style.css"
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FormControlLabel, Checkbox } from '@material-ui/core';
+import "../style.css";
 
-class Renown extends Component {
-    state ={
-        playerInfo: this.props.campaign.playerData,
-        playerOrgs: []
-    }
+const Renown = () => {
+    const [playerInfo, setPlayerInfo] = useState(this.props.campaign.playerData);
+    const [playerOrgs, setPlayerOrgs] = useState([]);
+    const [showModal, setShowModal] = useState(false);
 
-    handleClick = () => {
-        this.setState({
-            showModal: !this.state.showModal,
-        })
-    }
-
-    handleChange = (event, index, icon) => {
-        let checked = event.target.checked
-        let name = event.target.name
+    const handleChange = (event, index, icon) => {
+        let checked = event.target.checked;
+        let name = event.target.name;
         
         if(checked) {
-            const playerOrgs = [...this.state.playerOrgs, {name:name, playerNumId:index, icon:icon}]
-            
-            this.setState({
-                playerOrgs: playerOrgs
-            })
+            const newPlayerOrgs = [...playerOrgs, {name:name, playerNumId:index, icon:icon}];
+            setPlayerOrgs(newPlayerOrgs);
         } else if (checked === false) {
-                const allPlayersFactions = this.state.playerOrgs.filter(item => (item.playerNumId !== index))
-                const playersFactions = this.state.playerOrgs.filter(item => (item.playerNumId === index))
-                const itemsByPlayerToKeep = playersFactions.filter(item => (item.name !== name))
-                const finalPlayerFactions = [...allPlayersFactions, ...itemsByPlayerToKeep]
-
-                this.setState({
-                    playerOrgs: finalPlayerFactions
-                })
+            const allPlayersFactions = playerOrgs.filter(item => (item.playerNumId !== index));
+            const playersFactions = playerOrgs.filter(item => (item.playerNumId === index));
+            const itemsByPlayerToKeep = playersFactions.filter(item => (item.name !== name));
+            const finalPlayerFactions = [...allPlayersFactions, ...itemsByPlayerToKeep];
+            setPlayerOrgs(finalPlayerFactions);
         }
     }
 
-    handleSave = () => {
-        const players = this.props.campaign.playerData
-        const playersByID = []
+    const handleSave = () => {
+        const players = this.props.campaign.playerData;
+        const playersByID = [];
 
         players.forEach((player, index) => {
-            player.factions = []
-            let playersFactions = this.state.playerOrgs.filter(item => (item.playerNumId === index))
-            player.factions.push(playersFactions)
-            playersByID.push(player) 
+            player.factions = [];
+            let playersFactions = this.state.playerOrgs.filter(item => (item.playerNumId === index));
+            player.factions.push(playersFactions);
+            playersByID.push(player) ;
         })
-
-        this.setState({
-            showModal: !this.state.showModal,
-        })
-
-        this.props.setPlayers("playerData", playersByID)
+        setShowModal(!showModal);
+        this.props.setPlayers("playerData", playersByID);
     }
 
-
-render() {  
     return (
         <div>
             <div className="btns">
-                <Button variant="outline-success" size="lg" onClick={this.handleClick}>Renown
-                </Button>
+                <Button variant="outline-success" size="lg" onClick={() => setShowModal(!showModal)}>Renown</Button>
             </div>
-            <Modal size="lg" show={this.state.showModal} onHide={this.handleClick}>
+            <Modal size="lg" show={showModal} onHide={() => setShowModal(!showModal)}>
                 <Modal.Header closeButton>
                     <Modal.Title>Do your characters have any renown with your factions/organizations?</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <p>Below select the renown that each character has with each faction (can be none).</p>
-                    
-                    <br></br>
-
-                    {this.state.playerInfo.length > 0 && (<div className="characterRenownContainer">
-                        {this.state.playerInfo.map((item, index) => {
-                            return <div>
-                                <div>{item.name}</div>
-                            {this.props.campaign.factionOrgs.map(item => {
-                                return <div>
-                                        <FontAwesomeIcon className="iconSpace" icon={item.icon}></FontAwesomeIcon>
-                                        <FormControlLabel
-                                        
-                                        control={
-                                        <Checkbox
-                                        onChange={(event) => this.handleChange(event, index, item.icon)} 
-                                        name={item.name}
-                                        value={item.name}
-                                        inputProps={item.icon}
-                                        color="primary"/>}
-                                        label={item.name}
-                                        />
-                                        
-                                 </div>
-                                 })}
-                            </div>
-                            })}
-                    </div>)}
-
-                    {this.state.playerInfo.length < 1 && (<div className="characterRenownContainer">
-                        <div>
-                            <p>You haven't added charcters to your campaign yet! If you're like to add certain renown to certain characters go back and a some characters in!</p>
+                    <br/>
+                    {playerInfo.length > 0 && (
+                        <div className="characterRenownContainer">
+                            {playerInfo.map((item, index) => {
+                                return (
+                                    <div>
+                                        <div>{item.name}</div>
+                                        {this.props.campaign.factionOrgs.map(item => {
+                                            return (
+                                                <div>
+                                                    <FontAwesomeIcon className="iconSpace" icon={item.icon}/>
+                                                    <FormControlLabel
+                                                        label={item.name} 
+                                                        control={
+                                                            <Checkbox
+                                                            onChange={(event) => handleChange(event, index, item.icon)} 
+                                                            name={item.name}
+                                                            value={item.name}
+                                                            inputProps={item.icon}
+                                                            color="primary"/>
+                                                        }/> 
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                )
+                                })}
                         </div>
-                    </div>)}
-
-
+                    )}
+                    {playerInfo.length < 1 && (
+                        <div className="characterRenownContainer">
+                            <div>
+                                <p>
+                                    You haven't added charcters to your campaign yet! 
+                                    If you're like to add certain renown to certain characters go back and a some characters in!
+                                </p>
+                            </div>
+                        </div>
+                    )}
                 </Modal.Body>
-
                 <Modal.Footer>
-                    <Button variant="outline-success" onClick={this.handleSave} >Save</Button>
+                    <Button variant="outline-success" onClick={handleSave} >Save</Button>
                 </Modal.Footer>
             </Modal>
         </div>
     );
-}
 }
 
 export default Renown;
