06e0d36039e9062c2bc3863f32abcaffc54b23e0
Molly Gilbert <msgilbert389@gmail.com>
2021-11-12 16:11:33 -0600

    refactoring languages

--- src/components/WorldBuilder/components/Languages.js
+++ src/components/WorldBuilder/components/Languages.js
@@ -1,71 +1,67 @@
-import React, {Component} from 'react'
-import Modal from 'react-bootstrap/Modal'
-import {Button, ButtonGroup, Dropdown, Form, FormControl, OverlayTrigger, Tooltip} from 'react-bootstrap'
-import {FormControlLabel, Checkbox, FormGroup, Slider} from '@material-ui/core'
-import "../style.css"
+import React, { useState  } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
+import "../style.css";
 
-class Languages extends Component {
-    state ={
-        value: "",
-        suggestedTags: [
-            "Celestial",
-            "Common",
-            "Draconic",
-            "Druidic",
-            "Dwarvish",
-            "Elvish",
-            "Infernal",
-        ]
+const Languages = () => {
+    const [value, setValue] = useState("");
+    const [suggestedTags, setSuggestedTags] = useState([
+        "Celestial",
+        "Common",
+        "Draconic",
+        "Druidic",
+        "Dwarvish",
+        "Elvish",
+        "Infernal",
+    ]);
+
+    const handleRemove = (event) => {
+        let removedItem = event.target.name;
+        let currentTags = suggestedTags;
+        const removedTag = currentTags.filter(item => item !== removedItem);
+        setSuggestedTags(removedTag);
+        this.props.setLanguages(suggestedTags);
+        this.props.setLanguageShow("languageShow", true);
     }
 
-    handleRemove = (event) => {
-        let removedItem = event.target.name
-        let currentTags = this.state.suggestedTags
-        const removedTag = currentTags.filter(item => item !== removedItem)
-        this.setState({
-            suggestedTags: removedTag
-        }, () => this.props.setLanguages(this.state.suggestedTags))
-        this.props.setLanguageShow("languageShow", true)
+    const handleAddLanguage = (event) => {
+        setSuggestedTags(suggestedTags.concat(value));
+        setValue("");
+        this.props.setLanguages(suggestedTags);
+        this.props.setLanguageShow("languageShow", true);
     }
 
-    handleAddLanguage = (event) => {
-        this.setState({
-            suggestedTags: this.state.suggestedTags.concat(this.state.value),
-            value: '',
-        }, () => this.props.setLanguages(this.state.suggestedTags))
-        this.props.setLanguageShow("languageShow", true)
-    }
-
-    handleInputChange = (event) => {
-        this.setState({
-            value: event.target.value
-        });
-    }
-
-    onKeyDown = (event) => {
+    const onKeyDown = (event) => {
         if (event.key === "Enter") {
-            this.handleAddLanguage();
-          }
+            handleAddLanguage();
+            }
     }
 
-render() {  
-    const{other} = this.state
     return (
         <div className="d-flex flex-column align-items-center">
-            <br></br>
+            <br/>
             <p>We have a few languages suggestions here. Feel free to add and remove as many or as little as you want.</p>
-
             <div className="tagsArea centeredItems">
-            {this.state.suggestedTags.map(item => {
-                return <ButtonGroup className="factionButtons"><Button name={item}>{item}</Button><Button name={item} onClick={this.handleRemove}>X</Button></ButtonGroup>
+            {suggestedTags.map(item => {
+                return (
+                    <ButtonGroup className="factionButtons">
+                        <Button name={item}>{item}</Button>
+                        <Button name={item} onClick={handleRemove}>X</Button>
+                    </ButtonGroup>
+                )
             })}
-                
-                <div className="inputAndBtn factionButtons">
-                    <input className="factionInput" placeholder="Language" type="text" value={this.state.value} onChange={this.handleInputChange} onKeyUp={(event) => this.onKeyDown(event)}></input>
-                    <Button size="sm" variant="outline-success" onClick={this.handleAddLanguage}>Submit</Button>
-                </div>
+            <div className="inputAndBtn factionButtons">
+                <input 
+                    className="factionInput" 
+                    placeholder="Language" 
+                    type="text" 
+                    value={value} 
+                    onChange={(e) => setValue(e.target.value)} 
+                    onKeyUp={(event) => onKeyDown(event)}/>
+                <Button size="sm" variant="outline-success" onClick={handleAddLanguage}>Submit</Button>
+            </div>
             </div>
         </div>
-    )}}
+    )
+}
 
 export default Languages;
