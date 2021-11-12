52257ef557ab5754df85c2cbd590fff740a0feb0
Molly Gilbert <msgilbert389@gmail.com>
2021-11-12 17:20:44 -0600

    refactor social

--- src/components/WorldBuilder/Social.js
+++ src/components/WorldBuilder/Social.js
@@ -1,58 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { Renown, FactionOrgs } from './components';
 import { Button } from 'react-bootstrap';
 import "../style.css";
 
-class Social extends Component {
+const Social = () => {
+    const [showModal, setShowModal] = useState(false);
 
-    state = {
-        showModal: false
-    }
-
-    handleClick = () => {
-        this.setState({
-            showModal: !this.state.showModal,
-        })
-    }
-
-
-render() {  
     return (
         <div>
             <div className="btns">
-                <Button variant="outline-success" size="lg" onClick={this.handleClick}>The Social
+                <Button variant="outline-success" size="lg" onClick={() => setShowModal(!showModal)}>The Social
                 </Button>
             </div>
-            <Modal size="lg" show={this.state.showModal} onHide={this.handleClick}>
+            <Modal size="lg" show={showModal} onHide={() => setShowModal(!showModal)}>
                 <Modal.Header closeButton>
                     <Modal.Title>Let's create some organizations that might exist in this world.</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>Use the buttons below to make some create those organizations and decide if your characters will have familiartiry with them.</p>
-                    
-                    <br></br>
-
+                    <p>
+                        Use the buttons below to make some create those organizations and decide if your characters will have familiartiry with them.
+                    </p>
+                    <br/>
                     <div className="btnspace">
                         <FactionOrgs
-                        setFactionOrgs={this.props.setFactionOrgs}
-                        setFactionShow={this.props.setFactionShow}
-                        ></FactionOrgs>
+                            setFactionOrgs={this.props.setFactionOrgs}
+                            setFactionShow={this.props.setFactionShow}/>
                         <Renown
-                        setPlayers={this.props.setPlayers}
-                        campaign={this.props.campaign}
-                        ></Renown>
+                            setPlayers={this.props.setPlayers}
+                            campaign={this.props.campaign}/>
                     </div>
-
                 </Modal.Body>
-
                 <Modal.Footer>
-                    <Button variant="outline-success" onClick={this.handleClick} >Save</Button>
+                    <Button variant="outline-success" onClick={() => setShowModal(!showModal)} >Save</Button>
                 </Modal.Footer>
             </Modal>
         </div>
     );
-}
 }
 
 export default Social;
