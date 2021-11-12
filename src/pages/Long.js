f3734c1916369f1030791bd2a753e068d2d49a1e
Molly Gilbert <msgilbert389@gmail.com>
2021-11-12 15:29:45 -0600

    fixing  other main btns

--- src/pages/Long.js
+++ src/pages/Long.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CampaignNameModal from "../components/WorldBuilder/CampaignNameModal"
+import { CampaignNameModal } from "../components/WorldBuilder";
 import '../components/home.css';
 
 function Long() {
@@ -9,14 +9,17 @@ function Long() {
             <div className="mainques">
                 <h1> Welcome to the Long Campaign Builder</h1>
                 <p>A Long campaign.</p>
-                <p>Let's talk characters. Let's think about the goal of this entire campaign. Do you want to start with your villain or your quest giver? Sometimes the quest giver and the villain are the same.</p>
-
+                <p>
+                    Let's talk characters. Let's think about the goal of this entire campaign. 
+                    Do you want to start with your villain or your quest giver? 
+                    Sometimes the quest giver and the villain are the same.
+                </p>
                 <div className="btnspace">
-                <CampaignNameModal></CampaignNameModal>
+                    <CampaignNameModal/>
                 </div>
             </div>
         </div>
     );
   }
-  
+
   export default Long;
--- src/pages/Medium.js
+++ src/pages/Medium.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CampaignNameModal from "../components/WorldBuilder/CampaignNameModal"
+import { CampaignNameModal } from "../components/WorldBuilder";
 import '../components/home.css';
 
 function Medium() {
@@ -9,10 +9,13 @@ function Medium() {
             <div className="mainques">
                 <h1> Welcome to the Long Campaign Builder</h1>
                 <p>A Long campaign.</p>
-                <p>Let's talk characters. Let's think about the goal of this entire campaign. Do you want to start with your villain or your quest giver? Sometimes the quest giver and the villain are the same.</p>
-
+                <p>
+                    Let's talk characters. Let's think about the goal of this entire campaign. 
+                    Do you want to start with your villain or your quest giver? 
+                    Sometimes the quest giver and the villain are the same.
+                </p>
                 <div className="btnspace">
-                <CampaignNameModal></CampaignNameModal>
+                    <CampaignNameModal/>
                 </div>
             </div>
         </div>
