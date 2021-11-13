28a6ef97b08c39ccc1e3d99fd510fe6b818a0cd7
Molly Gilbert <msgilbert389@gmail.com>
2021-11-12 20:57:08 -0600

    adding dungeon

--- src/components/Dungeon/Rooms.js
+++ src/components/Dungeon/Rooms.js
@@ -1,42 +1,27 @@
-import React, { Component } from 'react'
-import Modal from 'react-bootstrap/Modal'
-import { Button } from 'react-bootstrap'
-import "./style.css"
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
+import "./style.css";
 
-class Rooms extends Component {
-    state ={
-        showModal: false,
-        roomNum: "",
-        roomOptions: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    }
+const Rooms = ({ campaign, setReduxDungeonData }) => {
+    const [showModal, setShowModal] = useState(false);
+    const [roomNum, setRoomNum] = useState("");
+    const roomOptions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-    handleClick = () => {
-        this.setState({
-            showModal: !this.state.showModal,
-        })
-    }
+    const handleActBtn = (event) => {
+        const choice = event.target.name;
+        const currentDungeonData = campaign.dungeonData;
+        const destructedData = {...currentDungeonData, rooms: choice};
+        setRoomNum(choice);
+        setShowModal(!showModal);
+        suggestMeMonsters();       
+        setReduxDungeonData("dungeonData", destructedData);
+    };
 
-    handleActBtn = (event) => {
-        const choice = event.target.name
-        const currentDungeonData=this.props.campaign.dungeonData
-        const destructedData = {...currentDungeonData, rooms:choice}
-        
-        this.setState({ 
-            roomNum: event.target.name,
-            showModal: !this.state.showModal
-        },() => this.suggestMeMonsters())
-
-       
-        this.props.setDungeonData("dungeonData", destructedData)
-    }
-
-    suggestMeMonsters() {
-        let roomNum = this.state.roomNum
-        roomNum = parseInt(roomNum)
-
-        let suggestedMonsterNum=0
-        let maxMonsterNum=0
-
+    function suggestMeMonsters() {
+        let roomNum = parseInt(roomNum)
+        let suggestedMonsterNum = 0
+        let maxMonsterNum = 0
         if (roomNum <= 5) {
             suggestedMonsterNum = 10
             maxMonsterNum = 20
@@ -47,21 +32,17 @@ class Rooms extends Component {
             suggestedMonsterNum = 4
             maxMonsterNum = 10
         }
-
-        const currentDungeonData=this.props.campaign.dungeonData
-        const destructedData = {...currentDungeonData, suggestedMonsterNum:suggestedMonsterNum, maxMonsterNum: maxMonsterNum}
-        this.props.setDungeonData("dungeonData", destructedData)
+        const currentDungeonData = campaign.dungeonData;
+        const destructedData = {...currentDungeonData, suggestedMonsterNum:suggestedMonsterNum, maxMonsterNum: maxMonsterNum};
+        setReduxDungeonData("dungeonData", destructedData);
     }
-
-render() {  
 
     return (
         <div>
             <div className="btns">
-                <Button id="" variant="outline-success" size="lg" onClick={this.handleClick}>Rooms
-                </Button>
+                <Button id="" variant="outline-success" size="lg" onClick={() => setShowModal(!showModal)}>Rooms</Button>
             </div>
-            <Modal show={this.state.showModal} onHide={this.handleClick}>
+            <Modal show={showModal} onHide={() => setShowModal(!showModal)}>
                 <Modal.Header closeButton>
                     <Modal.Title>Name that Room!</Modal.Title>
                 </Modal.Header>
@@ -69,16 +50,18 @@ render() {
                     <p>How many rooms are in this dungeon.</p>
                     <p>This is your custom campaign, have as many rooms as you'd like!</p>
                     <div className="roomSpace">
-                        {this.state.roomOptions.map(item => {return <div className="roomBtns">
-                            <Button name={item} variant="outline-primary" onClick={this.handleActBtn}>{item}</Button>
-                        </div>}
+                        {roomOptions.map(item => {
+                            return (
+                                <div className="roomBtns">
+                                    <Button name={item} variant="outline-primary" onClick={handleActBtn}>{item}</Button>
+                                </div>
+                            )}
                         )}
                     </div>
                 </Modal.Body>
             </Modal>
         </div>
     );
-}
 }
 
 export default Rooms;
