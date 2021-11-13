578644fd7596b83fa0f121580ce68de90d57a950
Molly Gilbert <msgilbert389@gmail.com>
2021-11-12 21:40:49 -0600

    adding more story

--- src/components/MoreStory/components/SideQuests.js
+++ src/components/MoreStory/components/SideQuests.js
@@ -1,146 +1,119 @@
-import React, { Component } from 'react'
-import Modal from 'react-bootstrap/Modal'
-import { Button, Dropdown } from 'react-bootstrap'
-import { Slider } from '@material-ui/core'
-import "../style.css"
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button, Dropdown } from 'react-bootstrap';
+import { Slider } from '@material-ui/core';
+import "../style.css";
 
-class SideQuests extends Component {
-    state = {
-        showModal: false,
-        yes: false, 
-        sideQuests: [],
-        sideQuestOptions: [
-            {id: 1, option: "Find a specific item rumored to be in the area"},
-            {id: 2, option: "Retrieve information from an NPC in the area"},
-            {id: 3, option: "Rescue a captive"},
-            {id: 4, option: "Discover the fate of a missing NPC"},
-            {id: 5, option: "Slay a specific monster"},
-            {id: 6, option: "Discover the nature and origin of a strange phenpmenon in the area"},
-            {id: 7, option: "Secure the aid of a character or creatue in the area"},
-            {id: 8, option: "Retrieve a stolen item in the villain's possession"},
-        ]
-    }
+const SideQuests = ({ setReduxSideQuests })  => {
+    const [showModal, setShowModal] = useState(false);
+    const [yes, setYes] = useState(false);
+    const [sideQuests, setSideQuests] = useState([]);
+    const sideQuestOptions = [
+        {id: 1, option: "Find a specific item rumored to be in the area"},
+        {id: 2, option: "Retrieve information from an NPC in the area"},
+        {id: 3, option: "Rescue a captive"},
+        {id: 4, option: "Discover the fate of a missing NPC"},
+        {id: 5, option: "Slay a specific monster"},
+        {id: 6, option: "Discover the nature and origin of a strange phenpmenon in the area"},
+        {id: 7, option: "Secure the aid of a character or creatue in the area"},
+        {id: 8, option: "Retrieve a stolen item in the villain's possession"},
+    ];
 
-    handleClick = () => {
-        this.setState({
-            showModal: !this.state.showModal,
-        })
-    }
-
-    handleYesNoSideQuest = (event) => {
+    const handleYesNoSideQuest = (event) => {
         let choice = event.target.name
-
         switch(choice) {
             case "Yes":
-                this.setState({
-                    yes: true,
-                })
+                setYes(true);
             break;
             default: 
-            this.setState({
-                sideQuests: [],
-                showModal: false,
-            })
+                setSideQuests([]);
+                setShowModal(false);
+            break;
         }
-    }
+    };
 
-    handleSideQuestChoice = (keyEvent, event, index) => {
-        const selection = event.target.text
-
+    const handleSideQuestChoice = (keyEvent, event, index) => {
+        const selection = event.target.text;
         const newSideQuest = [...this.state.sideQuests].map(item => {
             if (item.id===index) {
                 return {...item, type:selection}
             } return item
-        })
-        
-        this.setState({
-            sideQuests: newSideQuest
-        })
+        });
+        setSideQuests(newSideQuest);
+        setReduxSideQuests("sideQuests", newSideQuest);
+    };
 
-        this.props.setSideQuests("sideQuests", newSideQuest)
-    }
-
-    handleSlider = (event, value) => {
-        let sideQuests = []
+    const handleSlider = (event, value) => {
+        let sideQuests = [];
         for (let i=0; i < value; i++) {
-            let newObject = {id:i}
-            sideQuests.push(newObject)
+            let newObject = {id:i};
+            sideQuests.push(newObject);
         }
+        setSideQuests(sideQuests);
+    };
 
-        this.setState({
-            sideQuests: sideQuests
-        })
-
-    }
-
-
-render() {  
     return (
         <div>
             <div className="btns">
-                <Button variant="outline-success" size="lg" onClick={this.handleClick}>Side Quests
-                </Button>
+                <Button variant="outline-success" size="lg" onClick={() => setShowModal(!showModal)}>Side Quests</Button>
             </div>
             <Modal
-            size="lg" 
-            show={this.state.showModal} 
-            onHide={this.handleClick}>
+                size="lg" 
+                show={showModal} 
+                onHide={() => setShowModal(!showModal)}>
                 <Modal.Header closeButton>
                     <Modal.Title>This is optional, do you want to add a side quest to your story?</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <p>Use the buttons below</p>
-                    
-                    <br></br>
-
+                    <br/>
                     <div className="sideQuestBtnSpace">
                         <div className="sideQuestBtns">
-                            <Button name="Yes" variant="outline-success" onClick={this.handleYesNoSideQuest}>Yes</Button>
+                            <Button name="Yes" variant="outline-success" onClick={handleYesNoSideQuest}>Yes</Button>
                         </div>
                         <div className="sideQuestBtns">
-                            <Button name="No" variant="outline-danger" onClick={this.handleYesNoSideQuest}>No</Button>
+                            <Button name="No" variant="outline-danger" onClick={handleYesNoSideQuest}>No</Button>
                         </div>
                     </div>
-
-                    <br></br>
-                    <br></br>
-
-                    {this.state.yes &&(<div>
-                        <Slider
-                        min={1}
-                        max={10}
-                        defaultValue={5}
-                        onChangeCommitted={this.handleSlider}
-                        valueLabelDisplay="on"
-                        />
-                    </div>)}
-
-                    {this.state.sideQuests.length > 0 && (<div>
-                        {this.state.sideQuests.map((space , index)=> {return <div>
-                        <Dropdown onSelect={(keyEvent, event) => this.handleSideQuestChoice(keyEvent, event, index)}>
-                            <Dropdown.Toggle variant="outline-primary">
-                            {this.state.sideQuests[index].type ? this.state.sideQuests[index].type: 'Choose your Side Quest Goal'}
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                            {this.state.sideQuestOptions.map(drop => {
-                                return <Dropdown.Item key={drop.id} name={drop.option}> {drop.option}</Dropdown.Item>
+                    <br/>
+                    <br/>
+                    {yes && (
+                        <div>
+                            <Slider
+                                min={1}
+                                max={10}
+                                defaultValue={5}
+                                onChangeCommitted={handleSlider}
+                                valueLabelDisplay="on"/>
+                        </div>
+                    )}
+                    {sideQuests.length > 0 && (
+                        <div>
+                            {sideQuests.map((space , index) => {
+                                return (
+                                    <div>
+                                        <Dropdown onSelect={(keyEvent, event) => handleSideQuestChoice(keyEvent, event, index)}>
+                                            <Dropdown.Toggle variant="outline-primary">
+                                                {sideQuests[index].type ? sideQuests[index].type: 'Choose your Side Quest Goal'}
+                                            </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                {sideQuestOptions.map(drop => {
+                                                    return <Dropdown.Item key={drop.id} name={drop.option}> {drop.option}</Dropdown.Item>
+                                                })}
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </div>
+                                )
                             })}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                        </div>})}
-                    </div>)}
-
-
+                        </div>
+                    )}
                 </Modal.Body>
-
                 <Modal.Footer>
-                    <Button variant="outline-success" onClick={this.handleClick} >Save</Button>
+                    <Button variant="outline-success" onClick={() => setShowModal(!showModal)} >Save</Button>
                 </Modal.Footer>
             </Modal>
         </div>
     );
-}
 }
 
 export default SideQuests;
--- src/components/MoreStory/components/Twists.js
+++ src/components/MoreStory/components/Twists.js
@@ -1,61 +1,44 @@
-import React, { Component } from 'react'
-import Modal from 'react-bootstrap/Modal'
-import { Button, Carousel } from 'react-bootstrap'
-import "../style.css"
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button, Carousel } from 'react-bootstrap';
+import "../style.css";
 
-class Twists extends Component {
+const Twists = ({ setReduxTwist }) => {
+    const [showModal, setShowModal] = useState(false);
+    const [twistYesNo, setTwistYesNo] = useState("");
+    const [twistChoice, setTwistChoice] = useState("");
+    const twists = [
+        {id: 1, option: "The adventurers are racing against other creatures with the same or opposite goals"},
+        {id: 2, option: "The adventurers become responsible for the safety of a noncombatant NPC"},
+        {id: 3, option: "The adventurers are prohibited from killing the villain, but the villain has no compunctions about killing them"},
+        {id: 4, option: "The adventurers have a time limit"},
+        {id: 5, option: "The adventurers have received false or extraneous information"},
+        {id: 6, option: "Completeting an adventure goal fulfills a prophecy or prevents the fulfillment of a prophecy"},
+        {id: 7, option: "The adventurers have two different goals, but they can only complete one"},
+        {id: 8, option: "Completing the goal secretly helps the villain"},
+        {id: 9, option: "The adventurers must cooperate with a known enemy to achieve a goal"},
+        {id: 10, option: "The adventurers are under magical compulsion to complete their goal"},
+    ];
 
-    state = {
-        showModal: false,
-        twistYesNo: '',
-        twistChoice: '',
-        twists: [
-            {id: 1, option: "The adventurers are racing against other creatures with the same or opposite goals"},
-            {id: 2, option: "The adventurers become responsible for the safety of a noncombatant NPC"},
-            {id: 3, option: "The adventurers are prohibited from killing the villain, but the villain has no compunctions about killing them"},
-            {id: 4, option: "The adventurers have a time limit"},
-            {id: 5, option: "The adventurers have received false or extraneous information"},
-            {id: 6, option: "Completeting an adventure goal fulfills a prophecy or prevents the fulfillment of a prophecy"},
-            {id: 7, option: "The adventurers have two different goals, but they can only complete one"},
-            {id: 8, option: "Completing the goal secretly helps the villain"},
-            {id: 9, option: "The adventurers must cooperate with a known enemy to achieve a goal"},
-            {id: 10, option: "The adventurers are under magical compulsion to complete their goal"},
-        ]
-    }
-
-    handleClick = () => {
-        this.setState({
-            showModal: !this.state.showModal,
-        })
-    }
-
-    handleYesNoTwists = (event) => {
-        let choice = event.target.name
-
+    const handleYesNoTwists = (event) => {
+        let choice = event.target.name;
         switch(choice) {
             case "Yes":
-                this.setState({
-                    twistYesNo: choice
-                })
+                setTwistYesNo(choice);
             break;
             default: 
-            this.setState({
-                twistYesNo: '',
-                twistChoice: '',
-            })
-            
+                setTwistYesNo("");
+                setTwistChoice("");
+            break;
         }
     }
 
-    handleTwist = (event) => {
-        const choice = event.target.name
-        this.setState({
-            twistChoice: choice
-        })
-        this.props.setTwist("twist", choice)
-    }
+    const handleTwist = (event) => {
+        const choice = event.target.name;
+        setTwistChoice(choice);
+        setReduxTwist("twist", choice);
+    };
 
-render() {  
     const style = {
         width: "350px",
         height: "125px",
@@ -63,52 +46,57 @@ render() {
         paddingTop: "10px",
         margin: "5px",
     }
+
     return (
         <div>
             <div className="btns">
-                <Button variant="outline-success" size="lg" onClick={this.handleClick}>Twists
-                </Button>
+                <Button variant="outline-success" size="lg" onClick={() => setShowModal(!showModal)}>Twists</Button>
             </div>
-            <Modal size="lg" show={this.state.showModal} onHide={this.handleClick}>
+            <Modal size="lg" show={showModal} onHide={() => setShowModal(!showModal)}>
                 <Modal.Header closeButton>
                     <Modal.Title>This is optional, do you want to add a twist to your story?</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <p>Do you want to add a twist?</p>
-                    
-                    <br></br>
-
+                    <br/>
                     <div className="sideQuestBtnSpace">
                         <div className="sideQuestBtns">
-                            <Button name="Yes" variant="outline-success" onClick={this.handleYesNoTwists}>Yes</Button>
+                            <Button name="Yes" variant="outline-success" onClick={handleYesNoTwists}>Yes</Button>
                         </div>
                         <div className="sideQuestBtns">
-                            <Button name="No" variant="outline-danger" onClick={this.handleYesNoTwists}>No</Button>
+                            <Button name="No" variant="outline-danger" onClick={handleYesNoTwists}>No</Button>
                         </div>
                     </div>
-
-                    {this.state.twistYesNo && (<div>
-
-                        <div className="d-flex flex-column align-items-center">
-                            <p>Click through these to add a fun twist to your story.</p>
-                            <Carousel interval={null}>
-                            {this.state.twists.map(drop => {
-                                return <Carousel.Item> <div className="d-block w-100"></div> <Button variant="primary" onClick={this.handleTwist} style={style} key={drop.id} name={drop.option}>{drop.option}</Button> </Carousel.Item>
-                            })}
-                            </Carousel>
+                    {twistYesNo && (
+                        <div>
+                            <div className="d-flex flex-column align-items-center">
+                                <p>Click through these to add a fun twist to your story.</p>
+                                <Carousel interval={null}>
+                                {twists.map(drop => {
+                                    return (
+                                        <Carousel.Item> 
+                                            <div className="d-block w-100"></div> 
+                                            <Button 
+                                                variant="primary" 
+                                                onClick={handleTwist} 
+                                                style={style} key={drop.id} 
+                                                name={drop.option}>
+                                                {drop.option}
+                                            </Button> 
+                                        </Carousel.Item>
+                                    )
+                                })}
+                                </Carousel>
+                            </div>
                         </div>
-                    </div>)}
-
-
+                    )}
                 </Modal.Body>
-
                 <Modal.Footer>
-                    <Button variant="outline-success" onClick={this.handleClick} >Save</Button>
+                    <Button variant="outline-success" onClick={() => setShowModal(!showModal)}>Save</Button>
                 </Modal.Footer>
             </Modal>
         </div>
     );
-}
 }
 
 export default Twists;
