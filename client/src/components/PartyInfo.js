49f2f0ed62d7b993f41111d8aa9c0e65469a2263
Molly Gilbert <msgilbert389@gmail.com>
2021-12-04 14:46:38 -0600

    adding fixed styling for party info

--- client/src/components/PartyInfo.js
+++ client/src/components/PartyInfo.js
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import update from "immutability-helper";
-import { Form, FloatingLabel, Container, Row, Col, Button, Dropdown } from "react-bootstrap";
+import {
+  Form,
+  FloatingLabel,
+  Container,
+  Row,
+  Col,
+  Button,
+  Dropdown,
+} from "react-bootstrap";
 import { setPlayers } from "../actions";
 import "./home.css";
 
@@ -23,7 +31,9 @@ const PartyInfo = ({ onSetPlayers }) => {
     "Warlock",
     "Wizard",
   ];
-  const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const levels = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
   const [firstDrop, setFirstDrop] = useState(true);
 
   const setReduxPlayers = (destination, value) => {
@@ -33,17 +43,16 @@ const PartyInfo = ({ onSetPlayers }) => {
   const handleClick = (e) => {
     let playerData = [];
     Array.from(Array(parseInt(e.target.id)).keys()).forEach((m, index) => {
-        const newObject = { id: index + 1, name: "", raceClass: "", level: 0 };
-        playerData.push(newObject);
-      }
-    );
+      const newObject = { id: index + 1, name: "", raceClass: "", level: 0 };
+      playerData.push(newObject);
+    });
     setPartyMemberData(playerData);
   };
 
   const handlePartyDetails = (ek, e, type) => {
     const jsEvent = e || ek;
     const index = jsEvent.target.id - 1;
-    console.log(partyMemberData)
+    console.log(partyMemberData);
     if (type === "name") {
       const newName = jsEvent.target.value;
       const newPartyMemberData = update(partyMemberData, {
@@ -58,31 +67,31 @@ const PartyInfo = ({ onSetPlayers }) => {
       });
       setPartyMemberData(newPartyMemberData);
     } else if (type === "level") {
-        const newLevel = jsEvent.target.text;
-        if(firstDrop) {
-            setFirstDrop(false);
-            const defaultLevels = partyMemberData.map((item, idx) => {
-                return {
-                    id: partyMemberData[idx].id,
-                    level: newLevel,
-                    name: partyMemberData[idx].name,
-                    raceClass: partyMemberData[idx].raceClass
-                }
-            })
-            setPartyMemberData(defaultLevels)
-        } else {
-            const newPartyMemberData = update(partyMemberData, {
-                [index]: {
-                  $set: {
-                    id: index + 1,
-                    name: partyMemberData[index].name,
-                    raceClass: partyMemberData[index].raceClass,
-                    level: newLevel,
-                  },
-                },
-              });
-              setPartyMemberData(newPartyMemberData);
-        }
+      const newLevel = jsEvent.target.text;
+      if (firstDrop) {
+        setFirstDrop(false);
+        const defaultLevels = partyMemberData.map((item, idx) => {
+          return {
+            id: partyMemberData[idx].id,
+            level: newLevel,
+            name: partyMemberData[idx].name,
+            raceClass: partyMemberData[idx].raceClass,
+          };
+        });
+        setPartyMemberData(defaultLevels);
+      } else {
+        const newPartyMemberData = update(partyMemberData, {
+          [index]: {
+            $set: {
+              id: index + 1,
+              name: partyMemberData[index].name,
+              raceClass: partyMemberData[index].raceClass,
+              level: newLevel,
+            },
+          },
+        });
+        setPartyMemberData(newPartyMemberData);
+      }
     } else if (type === "race") {
       const newRace = jsEvent.target.name;
       const newPartyMemberData = update(partyMemberData, {
@@ -111,33 +120,25 @@ const PartyInfo = ({ onSetPlayers }) => {
       </p>
       <div className="partyBtnSpace">
         {quantity.map((number, idx) => (
-            <div key={idx} className="prtyBtns">
-              <Button
-                id={number}
-                variant="outline-primary"
-                onClick={handleClick}>
-                {number}
-              </Button>
-            </div>
-          ))}
+          <div key={idx} className="prtyBtns">
+            <Button id={number} variant="outline-primary" onClick={handleClick}>
+              {number}
+            </Button>
+          </div>
+        ))}
       </div>
       {partyMemberData.length > 0 && (
         <div>
           <div className="forms">
             {[...partyMemberData].map((partyMember, index) => (
-                <div key={partyMember.id}>
-                  <Form inline>
-                    <Container md className="justify-content-center" style={{ margin: 5}}>
-                      <Row>
-                      <Col className="align-self-center">
-                      <FloatingLabel sm label={`Party Member ${partyMember.id}`}>
-                        <Form.Control sm placeholder="Name" id={partyMember.id} onChange={(ek, e) => handlePartyDetails(ek, e, "name")}/>
-                      </FloatingLabel>
-                      </Col>
-                      <Col md className="align-self-center">
-                      <Dropdown
-                        onSelect={(ek, e) => handlePartyDetails(ek, e, "race")}
-                        id={partyMember.id}>
+              <div key={partyMember.id}>
+                <Form style={{ marginBottom: 5}}>
+                  <Row>
+                    <Col align-self-center>
+                        <Form.Control sm placeholder={`Party Member ${partyMember.id}`} id={partyMember.id} onChange={(ek, e) => handlePartyDetails(ek, e, "name")}/>
+                    </Col>
+                    <Col md align-self-center>
+                      <Dropdown onSelect={(ek, e) => handlePartyDetails(ek, e, "race")}>
                         <Dropdown.Toggle variant="outline-primary">
                           {partyMemberData[index].raceClass ? partyMemberData[index].raceClass : "Choose a Class for this Party Member"}
                         </Dropdown.Toggle>
@@ -152,30 +153,25 @@ const PartyInfo = ({ onSetPlayers }) => {
                           ))}
                         </Dropdown.Menu>
                       </Dropdown>
-                      </Col>
-                      <Col md className="align-self-center">
-                      <Dropdown
-                        onSelect={(ek, e) => handlePartyDetails(ek, e, "level")}
-                        id={partyMember.id}>
+                    </Col>
+                    <Col md align-self-center>
+                      <Dropdown onSelect={(ek, e) => handlePartyDetails(ek, e, "level")}>
                         <Dropdown.Toggle variant="outline-primary">
                           {partyMemberData[index].level ? partyMemberData[index].level : "Choose a level for this party member"}
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
                           {levels.map((item, idx) => (
-                            <Dropdown.Item 
-                                key={idx} 
-                                id={partyMember.id}>
+                            <Dropdown.Item key={idx} id={partyMember.id}>
                               {item}
                             </Dropdown.Item>
                           ))}
                         </Dropdown.Menu>
                       </Dropdown>
-                      </Col>
-                    </Row>
-                    </Container>
-                  </Form>
-                </div>
-              ))}
+                    </Col>
+                  </Row>
+                </Form>
+              </div>
+            ))}
           </div>
           <div className="saveBtn">
             <Button variant="outline-success" onClick={handleReduxSave}>
